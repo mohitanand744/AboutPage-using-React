02ea7ad8144b1be5c70a8f02c39c1d49d43d2255
Mohit Jha <95844984+mohitanand744@users.noreply.github.com>
2024-08-07 19:51:35 +0530

    added new section in Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -45,45 +45,73 @@ function Home() {
         </header>
 
         <div className="container-fluid m-auto pd d-flex justify-content-center flex-row flex-wrap gap-5">
-          <div class="card p-2">
-            <div class="card-body">
+          <div className="card p-2">
+            <div className="card-body">
               <img src="./men.png" className="mt-4" alt="" />
-              <h2 class="card-title fs-2 mt-4 mb-3">State Your Problem</h2>
-              <p class="card-text fs-3 mb-4">
+              <h2 className="card-title fs-2 mt-4 mb-3">State Your Problem</h2>
+              <p className="card-text fs-3 mb-4">
                 Describe your goals or challenges, and our algorithm will find a
                 suitable match for you.
               </p>
             </div>
           </div>
-          <div class="card p-2 ">
-            <div class="card-body">
+          <div className="card p-2 ">
+            <div className="card-body">
               <img src="./men.png" className="mt-4" alt="" />
-              <h2 class="card-title fs-2 mt-4 mb-3">Filter to Get the Best</h2>
-              <p class="card-text fs-3 mb-4">
+              <h2 className="card-title fs-2 mt-4 mb-3">
+                Filter to Get the Best
+              </h2>
+              <p className="card-text fs-3 mb-4">
                 Swipe left or right and answer a few questions to refine your
                 search based on your interests.
               </p>
             </div>
           </div>
-          <div class="card p-2 ">
-            <div class="card-body">
+          <div className="card p-2 ">
+            <div className="card-body">
               <img src="./men.png" className="mt-4" alt="" />
-              <h2 class="card-title fs-2 mt-4 mb-3">Know Your Coach</h2>
-              <p class="card-text fs-3 mb-4">
+              <h2 className="card-title fs-2 mt-4 mb-3">Know Your Coach</h2>
+              <p className="card-text fs-3 mb-4">
                 Explore coach activities such as shorts, social feeds, live
                 sessions, and short listings.
               </p>
             </div>
           </div>
-          <div class="card p-2 ">
-            <div class="card-body">
+          <div className="card p-2 ">
+            <div className="card-body">
               <img src="./men.png" className="mt-4" alt="" />
-              <h2 class="card-title fs-2 mt-4 mb-3">Connect with Your Coach</h2>
-              <p class="card-text fs-3 mb-4">
+              <h2 className="card-title fs-2 mt-4 mb-3">
+                Connect with Your Coach
+              </h2>
+              <p className="card-text fs-3 mb-4">
                 Explore coach activities such as shorts, social feeds, live
                 sessions.
               </p>
             </div>
+          </div>
+        </div>
+      </div>
+
+      {/* __________________________________________ */}
+
+      <div className="container-fluid pd whyQoacha">
+        <div className=" d-flex gap-5 justify-content-center justify-content-sm-start  flex-wrap">
+          <div className="image_Container">
+            <img src="./bigLogo.png" alt="" />
+          </div>
+          <div className="txt_Container d-flex flex-column align-items-start gap-4">
+            <p className="fs-2 l-s">WHY QOACH</p>
+            <h1>
+              Connect with coaches who've been through what you're facing and
+              can guide you to success.
+            </h1>
+            <p className="fs-3 text-dark p-4 bg">
+              Lorem ipsum dolor sit amet consectetur. Tempus amet mauris velit
+              sed. Non sodales diam non viverra dui.
+            </p>
+            <button className="btn btn-outline-dark text-white p-3 fs-2 border-3 ">
+              Join Demo
+            </button>
           </div>
         </div>
       </div>
